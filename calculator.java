ff3fa0316e7b388ff060961a88188a1a600ad205
sanjeev098 <102794782+sanjeev098@users.noreply.github.com>
2022-04-08 12:28:45 +0530

    Add files via upload

---
+++ calculator.java
@@ -0,0 +1,21 @@
+package com.capg.cal;
+public class calculator {
+	public static void main(String[]args) { 
+	int a=30;
+	int b=25;
+	int num3=a+b;
+	int num4=a*b;
+	int num5=a/b;
+	int num6=a-b;
+	int  num7 =a%b;
+     System.out.println("addition"+num3);
+     System.out.println("multiplication is"+num4);
+     System.out.println("divisionis"+num5);
+     System.out.println("substraction is"+num6);    
+     System.out.println("modulus is"+num7);
+		
+	}
+	
+	}
+
+
